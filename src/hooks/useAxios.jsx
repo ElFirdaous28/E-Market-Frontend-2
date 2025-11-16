f98561e456eaa9d30261cbc3cf91939c1394daa9
ElFirdaous28 <elmokhtarifirdaous@gmail.com>
2025-11-16 23:45:59 +0100

    EMF2-28 carSlice and useCart hook

--- src/hooks/useAxios.jsx
+++ src/hooks/useAxios.jsx
@@ -1,16 +1,23 @@
 import { useAuth } from "../hooks/useAuth";
 import { useMemo } from "react";
 import api from "../services/axios";
+import { v4 as uuid } from "uuid";
 
 export const useAxios = () => {
     const { accessToken } = useAuth();
+    let sessionId = localStorage.getItem("sessionId");
 
+    if (!sessionId) {
+        sessionId = uuid();
+        localStorage.setItem("sessionId", sessionId);
+    }
     // useMemo ensures we don't recreate instance on every render unnecessarily
     const axiosInstance = useMemo(() => {
         const instance = api;
 
         instance.interceptors.request.use(
             (config) => {
+                config.headers["session-id"] = sessionId;
                 if (accessToken) {
                     config.headers.Authorization = `Bearer ${accessToken}`;
                 }
