ea9ec2395d11b88c8ac63be3ef4e689bac478316
ElFirdaous28 <elmokhtarifirdaous@gmail.com>
2025-11-23 18:14:19 +0100

    EMF2-35 login integration tests

--- setupTests.js
+++ setupTests.js
@@ -4,3 +4,24 @@ import { TextEncoder, TextDecoder } from "util";
 
 global.TextEncoder = TextEncoder;
 global.TextDecoder = TextDecoder;
+global.importMeta = {
+    env: {
+        VITE_API_URL: 'http://localhost:3000',
+    },
+};
+
+// 1. Mock React Toastify
+jest.mock("react-toastify", () => ({
+    toast: {
+        success: jest.fn(),
+        error: jest.fn(),
+        warn: jest.fn(),
+        info: jest.fn(),
+    },
+}));
+
+// 2. Mock React Router Dom
+jest.mock("react-router-dom", () => ({
+    ...jest.requireActual("react-router-dom"),
+    useNavigate: jest.fn(),
+}));
---
+++ src/services/__mocks__/axios.js
@@ -0,0 +1,12 @@
+const mockAxios = {
+    post: jest.fn(() => Promise.resolve({ data: {} })),
+    get: jest.fn(() => Promise.resolve({ data: {} })),
+    put: jest.fn(() => Promise.resolve({ data: {} })),
+    delete: jest.fn(() => Promise.resolve({ data: {} })),
+    interceptors: {
+        request: { use: jest.fn(), eject: jest.fn() },
+        response: { use: jest.fn(), eject: jest.fn() },
+    },
+};
+
+export default mockAxios;
