eddae15caab636c5c676ddfad23caeea718cac3f
ayoubjebb2001 <jebbouri1968@gmail.com>
2025-11-24 16:51:52 +0100

    feat: implement coupon management features with create and edit functionality

--- src/routes/SellerRoutes.jsx
+++ src/routes/SellerRoutes.jsx
@@ -5,6 +5,8 @@ import CreateProduct from "../pages/seller/CreateProduct";
 import EditProduct from "../pages/seller/EditProduct";
 import OrdersManager from "../pages/seller/OrdersManager";
 import CouponsManager from "../pages/seller/CouponsManager";
+import CouponCreate from "../pages/seller/CouponCreate";
+import CouponEdit from "../pages/seller/CouponEdit";
 
 export default function SellerRoutes() {
     return (
@@ -16,6 +18,8 @@ export default function SellerRoutes() {
             <Route path="/seller/products/edit/:id" element={<EditProduct />} />
             <Route path="/seller/orders" element={<OrdersManager />} />
             <Route path="/seller/coupons" element={<CouponsManager />} />
+            <Route path="/seller/coupons/create" element={<CouponCreate />} />
+            <Route path="/seller/coupons/edit/:id" element={<CouponEdit />} />
         </>
     );
 }
